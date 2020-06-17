41791d79e18bc8be86777bee5fede2109a7240a7
Abdallah <eng.abdallahobaid@gmail.com>
2020-06-17 06:05:10 +0300

    done

--- driver.js
+++ driver.js
@@ -7,6 +7,6 @@ events.on('pickup', payload => handleplaplapla(payload));
 function handleplaplapla(payload) {
   setTimeout(function(){console.log(`DRIVER: picked up ${payload.id}`);
     events.emit('transit', payload);},1000);
-  setTimeout(function(){console.log(`DRIVER: picked up ${payload.id}`);
+  setTimeout(function(){console.log(`DRIVER: delivered ${payload.id}`);
     events.emit('delivered', payload); }, 3000);
 }
